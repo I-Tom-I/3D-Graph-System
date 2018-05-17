59d355b61fcee647a3370cf91581117055d53d4e
Tom-III <brogan.tom.iii@gmail.com>
2018-05-17 12:03:39 +0200

    Initial commit

---
+++ Assets/BillboardCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BillboardCamera : MonoBehaviour
+{
+    [Header("Set which camera the Sprite will billboard to")]
+    public Camera camera;
+    void Update()
+    {
+        transform.LookAt(transform.position + camera.transform.rotation * Vector3.forward, camera.transform.rotation * Vector3.up);
+    }
+}
---
+++ Assets/ScrollWheelScaler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollWheelScaler : MonoBehaviour {
+
+    public float scaleMin = 0.5f;
+    public float scaleMax = 2.0f;
+    public float currentScale = 0.81f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentScale = Mathf.Clamp(currentScale - Input.GetAxis("Mouse ScrollWheel") * 2.5f, scaleMin, scaleMax);
+        transform.localScale = new Vector3(currentScale, currentScale, currentScale);
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            currentScale = 0.81f;
+        }
+    }
+}
